a904c0d0892bf7b47f299c050c5ca145d512dae6
Pallagi Joe <pallagijoe@gmail.com>
2023-09-09 14:36:33 +0200

    update cors settings

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://contact-form-client.web.app'],
+    origin: [
+      'http://localhost:4200',
+      'http://localhost:8100',
+      'https://contact-form-client.web.app',
+    ],
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
     credentials: true,
   });
